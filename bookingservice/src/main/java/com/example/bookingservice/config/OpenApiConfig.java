9f0e35f23e8044ddf15802cce0d6c5b4ad682aba
Naveesha Lakshan <lakshannaveesha10@gmail.com>
2026-02-26 21:32:57 +0530

    Ticket Booking - 3 Microservices

---
+++ bookingservice/src/main/java/com/example/bookingservice/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.example.bookingservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI inventoryServiceApi() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Booking Service API")
+                        .description("API documentation for the Booking Service")
+                        .version("1.0.0"));
+    }
+}
